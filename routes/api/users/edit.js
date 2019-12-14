1170647b419ddd44e255a37a89d4f4a3d04f3bb1
User 1 <stud@JS1>
2019-12-14 21:58:26 +0700

    user edit

--- routes/api/users/edit.js
+++ routes/api/users/edit.js
@@ -1,54 +1,69 @@
 const express = require('express')
-
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const router = express.Router()
-const bcrypt = require('bcryptjs')
 const { ObjectID } = require('mongodb')
 
 const User = require('../../../models/User')
 
 router.post('/', (req, res) => {
-  const { id, avatar_url, name, email, password } = req.body
-
-  User.findById(ObjectID(id))
-    .then(user => {
-      bcrypt
-        .compare(password, user.password)
-        .then(isMatch => {
-          if (isMatch) {
-            if (avatar_url) {
-              User.updateOne(
-                { _id: ObjectID(id) },
-                { $set: { avatar_url } },
-                err => {
-                  if (err) console.log(err)
-                },
-              )
-            }
-            if (name) {
-              User.updateOne({ _id: ObjectID(id) }, { $set: { name } }, err => {
-                if (err) console.log(err)
-              })
-            }
-            if (email) {
-              User.updateOne(
-                { _id: ObjectID(id) },
-                { $set: { email } },
-                err => {
-                  if (err) console.log(err)
-                },
-              )
-            }
-            res.json({
-              status: 'success',
-            })
-          }
-        })
-        .catch(() =>
-          res.json({
-            status: 'failed',
-            error: 'password incorrect',
-          }),
+  const { avatar_url, name, email, description, token } = req.body
+  const decoded = jwt.verify(token, config.get('jwtSecret'))
+  
+  User.findOne({ _id: ObjectID(decoded.id) })
+    .then(() => {
+      if (avatar_url) {
+        User.updateOne(
+          { _id: ObjectID(decoded.id) },
+          { $set: { avatar_url } },
+          err => {
+            if (err) console.log(err)
+          },
         )
+      }
+      if (name) {
+        User.updateOne(
+          { _id: ObjectID(decoded.id) }, 
+          { $set: { name } }, 
+          err => {
+            if (err) console.log(err)
+          }
+        )
+      }
+      if (email) {
+        User.updateOne(
+          { _id: ObjectID(decoded.id) },
+          { $set: { email } },
+          err => {
+            if (err) console.log(err)
+          }
+        )
+      }
+      if (description) {
+        User.updateOne(
+          { _id: ObjectID(decoded.id) }, 
+          { $set: { description } }, 
+          err => {
+            if (err) console.log(err)
+          }
+        )
+      }
+    })
+    .then(() => {
+      User.findOne({ _id: ObjectID(decoded.id) })
+        .then(user => {
+          res.json({
+            user: {
+              id: user.id,
+              avatar_url: user.avatar_url,
+              name: user.name,
+              email: user.email,
+              description: user.description,
+              liked_posts: user.liked_posts,
+              following: user.following,
+            },
+          })
+        })
     })
     .catch(() =>
       res.json({
